b3fab1af2e956ea811037ee359e5cd5440def353
rikisaraan <rikisaraan2@gmail.com>
2024-05-13 13:03:24 +0700

    feat: add laravel echo to view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\HelloEvent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/sender', function () {
+    broadcast(new HelloEvent(date('Y-m-d H:i:s') . " Selamat Datang"));
+});
+
+
+Route::get('/home', function () {
+    return view('home');
 });
